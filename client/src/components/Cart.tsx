81c5cba14eb444f3e343e93fe517066d951c2d3c
Ibrahim Khalilov <ibrahim.khalilov@yahoo.com>
2024-11-06 21:24:26 -0500

    need to make in cents

--- client/src/components/Cart.tsx
+++ client/src/components/Cart.tsx
@@ -10,12 +10,21 @@ const Cart = () => {
   const { cart, dispatch } = useContext(CartContext);
   const navigate = useNavigate();
 
+  const subtotal = () => {
+    let sum = 0;
+    for (const item of cart) {
+      sum += item.items.price * item.quantity;
+    }
+    return sum.toFixed(2);
+  };
+
   return (
     <div className="cart-container">
       <h1 className="cart-heading">Items in Your Cart</h1>
       {cart.length !== 0 ? (
         <>
           <CartTable />
+          <div className="total">Total: $ {subtotal()}</div>
           <div className="btn-container">
             <div>
               <button
